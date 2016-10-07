79e464c77659ded76167bb2e0a5de0ba72e94476
pihizi <pihizi@msn.com>
2016-10-07 13:37:53 +0000

    这个是临时的修改，源库没有提供http方式的访问，我这么修改只是为了满足自身产品的需求

--- source/CAS/Client.php
+++ source/CAS/Client.php
@@ -314,7 +314,7 @@ class CAS_Client
     {
         // the URL is build only when needed
         if ( empty($this->_server['base_url']) ) {
-            $this->_server['base_url'] = 'https://' . $this->_getServerHostname();
+            $this->_server['base_url'] =  ($this->_isHttps() ? 'https://' : 'http://') . $this->_getServerHostname();
             if ($this->_getServerPort()!=443) {
                 $this->_server['base_url'] .= ':'
                 .$this->_getServerPort();
